447aa7edd95cf6531943f6c77b245b4efb6a04d5
MarcosVB <mvberte@hotmail.com>
2021-10-30 15:36:35 -0300

    Added byte key generation and e expansion

--- src/cipher/KeyScheduler.java
+++ src/cipher/KeyScheduler.java
@@ -1,5 +1,7 @@
 package cipher;
 
+import java.util.Arrays;
+
 import utils.Bitwise;
 
 public class KeyScheduler {
@@ -20,10 +22,45 @@ public class KeyScheduler {
 	return keys;
     }
 
-    public static byte[] circularShift(byte[] array, int shift) {
+    public static byte[][] generateKeys(byte[] key, int shiftSize, int keySize, int amount) {
+	byte[] sourceKey = Arrays.copyOf(key, key.length);
+	byte[][] keys = new byte[amount][keySize];
+
+	sourceKey = expandKey(sourceKey, keySize);
+
+	for (int i = 0; i < keys.length; i++)
+	    keys[i] = (sourceKey = circularShift(sourceKey, shiftSize));
+
+	return keys;
+    }
+
+    public static byte[] expandKey(byte[] key, int size) {
+	if (key.length > size)
+	    throw new IllegalArgumentException("Key is bigger than defined size: " + key.length + " > " + size);
+
+	if (key.length == size)
+	    return key;
+
+	byte[] expandedKey = Arrays.copyOf(key, size);
+
+	int i = key.length;
+	int j = 0;
+
+	while (i < size) {
+	    if (j >= key.length)
+		j = 0;
+
+	    expandedKey[i++] = key[j++];
+	}
+
+	return expandedKey;
+    }
+
+    public static byte[] circularShift(byte[] source, int shift) {
 	int remainingShift = shift;
 	int currentShift;
 	byte bitsFromFirstByte;
+	byte[] array = Arrays.copyOf(source, source.length);
 
 	while (remainingShift > 0) {
 	    currentShift = remainingShift < Byte.SIZE ? remainingShift : Byte.SIZE - 1;
